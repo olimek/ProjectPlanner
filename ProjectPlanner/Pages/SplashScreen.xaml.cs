3b4f144f4987bf4f62c376f2c897034c28abc908
Michal Olichwer <72801083+olimek@users.noreply.github.com>
2025-08-18 19:41:35 +0200

    start

---
+++ ProjectPlanner/Pages/SplashScreen.xaml.cs
@@ -0,0 +1,47 @@
+#if ANDROID
+
+using Android.Views;
+
+#endif
+
+namespace ProjectPlanner.Pages;
+
+public partial class SplashScreen : ContentPage
+{
+    public SplashScreen()
+    {
+        InitializeComponent();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+#if ANDROID
+        var window = Platform.CurrentActivity?.Window;
+        if (window != null)
+        {
+            window.DecorView.SystemUiVisibility =
+                (StatusBarVisibility)(
+                    SystemUiFlags.HideNavigation
+                    | SystemUiFlags.Fullscreen
+                    | SystemUiFlags.ImmersiveSticky
+                );
+        }
+#endif
+        await Task.Delay(1500);
+        Application.Current.MainPage = new AppShell();
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+
+#if ANDROID
+        var window = Platform.CurrentActivity?.Window;
+        if (window != null)
+        {
+            window.DecorView.SystemUiVisibility = 0; // przywraca paski
+        }
+#endif
+    }
+}
