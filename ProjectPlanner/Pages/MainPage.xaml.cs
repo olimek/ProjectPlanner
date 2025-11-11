f87c48c69930d5be40a47adcb9a91d8e94f4941c
Olichwer <Olichwer@DESKTOP-U85PK5S>
2025-11-11 14:56:48 +0100

    dfgdgdg

--- ProjectPlanner/Pages/MainPage.xaml.cs
+++ ProjectPlanner/Pages/MainPage.xaml.cs
@@ -1,9 +1,9 @@
-﻿
+﻿using ProjectPlanner.Model;
 using ProjectPlanner.Data.UnitOfWork;
 
 using ProjectPlanner.ViewModels;
 
-namespace ProjectPlanner
+namespace ProjectPlanner.Pages
 {
     public partial class MainPage : ContentPage
     {
@@ -12,6 +12,14 @@ namespace ProjectPlanner
            
             InitializeComponent();
             BindingContext = new MainPageViewModel(unitOfWork);
+        }
+        private async void OnProjectSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection.FirstOrDefault() is Project selectedProject)
+            {
+                await Navigation.PushAsync(new ProjectDetailsPage(selectedProject));
+                ((CollectionView)sender).SelectedItem = null;
+            }
         }
 
     }
