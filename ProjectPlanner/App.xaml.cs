63ebf87c3dc96f20e43dc96f90808356b9f33d73
Michal Olichwer <72801083+olimek@users.noreply.github.com>
2025-12-05 11:27:45 +0100

    replaced splashscreen

--- ProjectPlanner/App.xaml.cs
+++ ProjectPlanner/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using ProjectPlanner.Pages;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectPlanner.Service;
 
 namespace ProjectPlanner
 {
@@ -12,7 +14,13 @@ namespace ProjectPlanner
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new SplashScreen());
+            // Resolve IProjectService from the DI container and start app at MainPage
+            if (MauiProgram.Services == null)
+                throw new InvalidOperationException("Application services are not available.");
+
+            var projectService = MauiProgram.Services.GetRequiredService<IProjectService>();
+            var main = new MainPage(projectService);
+            return new Window(main);
         }
     }
 }
